d982fb6a74828351b3e064af07ef22988c3cf085
pengxl <pengxljava@163.com>
2019-06-26 11:33:47 +0800

    修改

--- deploy.sh
+++ deploy.sh
@@ -16,29 +16,43 @@
 ## NAME Jar名称
 ## VERSION Jar版本
 
-#指定最后编译好的jar存放的位置
-WWW_PATH=/home/jars
 
 #Jenkins中编译好的jar位置
 JAR_PATH=${PROJECT_PATH}/target
 
-#获取运行编译好的进程ID，便于我们在重新部署项目的时候先杀掉以前的进程
-PID=$(cat /home/pidfiles/activiti.pid)
+#指定最后编译好的jar存放的位置
+WWW_PATH=/home/jars
 
-#输出pid
-echo ${PID}
+mkdirFolder(){
+    if [ ! -d $WWW_PATH ];then
+        mkdir $WWW_PATH
+    else
+        echo "文件夹已经存在"
+    fi
+}
 
-#进入指定的编译好的jar的位置
-cd ${JAR_PATH}
+mkdirFolder
+
+killServer(){
+    #获取运行编译好的进程ID，便于我们在重新部署项目的时候先杀掉以前的进程
+    pid=$(cat /home/pidfiles/activiti.pid)
+    echo "tomcat Id list :$pid"
+    if [ "$pid" = "" ]
+    then
+      echo "no tomcat pid alive"
+    else
+      kill -9 $pid
+    fi
+}
 
 #将编译好的jar复制到最后指定的位置
 cp ${JAR_PATH}/${NAME}-${VERSION}.jar ${WWW_PATH}
 
+#杀掉以前可能启动的项目进程
+killServer
+
 #进入最后指定存放jar的位置
 cd ${WWW_PATH}
-
-#杀掉以前可能启动的项目进程
-kill -9 ${PID}
 
 #启动jar，指定SpringBoot的profiles为test,后台启动
 #java -jar -Dspring.profiles.active=test ${jar_name} &
